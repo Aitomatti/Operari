260347b47c4ce4202684b38283c238ca597f2968
Marko Tiitto <mtiitto@edu.lapinamk.fi>
2021-11-08 15:27:17 +0200

    Create quarterionGyro_ESP32.ino
    Gyro data lähetys lisätty

---
+++ quarter_BLEGATTARDUINO/quarterionGyro_ESP32.ino
@@ -0,0 +1,282 @@
+/*
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleWrite.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+
+    https://forum.arduino.cc/t/getting-quaternion-values-from-mpu6050/668823
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+//Gyro/accel
+#include "SparkFunLSM6DS3.h"
+#include "Wire.h"
+
+//Create a instance of class LSM6DS3
+LSM6DS3 myIMU(I2C_MODE, 0x6A);
+
+int MPU_addr = 0x68;
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+float A_cal[6] = {264.0, -80.0, -700.0, 0.994, 1.000, 1.014}; // 0..2 offset xyz, 3..5 scale xyz
+float G_off[3] = { -499.5, -37.7, -89.0}; //raw offsets, determined for gyro at rest
+#define gscale ((250./32768.0)*(PI/180.0))  //gyro default 250 LSB per d/s -> rad/s
+
+// vector to hold quaternion
+float q[4] = {1.0, 0.0, 0.0, 0.0};
+
+
+//Lähetettävä data paketti
+struct DataPackage {
+  float x;
+  float y;
+  float z;
+  float w;
+  int buttons;
+};
+
+
+// Kp for proportional feedback, Ki for integral
+float Kp = 30.0;
+float Ki = 0.0;
+ char s[60]; //snprintf buffer, if needed
+
+// globals for AHRS loop timing
+unsigned long now_ms, last_ms = 0; //millis() timers
+
+// print interval
+unsigned long print_ms = 200; //print angles every "print_ms" milliseconds
+float yaw, pitch, roll; //Euler angle output
+
+bool deviceConnected = false;
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+
+BLECharacteristic *pCharacteristic ;
+
+const int PushButton = 16;
+
+enum buttonstate {
+  one = 0b0001,
+  two = 0b0010,
+  three = 0b0100
+};
+
+
+void setup() {
+  
+  Serial.begin(115200);
+
+  Serial.println("Marko Tiitto");
+
+  BLEDevice::init("TiittoESP32");
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  pCharacteristic->addDescriptor(new BLE2902());
+  pService->start();
+  pServer->getAdvertising()->start();
+
+  pinMode(PushButton, INPUT);
+
+  //Grove 6-axis setup
+      if (myIMU.begin() != 0) {
+        Serial.println("Device error");
+    } else {
+        Serial.println("Device OK!");
+    }
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+}
+
+void loop() {
+
+  static int i = 0;
+  static float deltat = 0;  //loop time in seconds
+  static unsigned long now = 0, last = 0; //micros() timers
+
+  //raw data
+  int32_t ax, ay, az;
+  int32_t gx, gy, gz;
+  int16_t Tmp; //temperature
+
+  //scaled data as vector
+  float Axyz[3];
+  float Gxyz[3];
+  
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
+  int t = Wire.read() << 8;
+  ax = myIMU.readFloatAccelX(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+
+  ay = myIMU.readFloatAccelY(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+
+  az = myIMU.readFloatAccelZ(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+
+  gx = myIMU.readFloatGyroX(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+
+  gy = myIMU.readFloatGyroY(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+
+  gz = myIMU.readFloatGyroZ(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  Axyz[0] = (float) ax;
+  Axyz[1] = (float) ay;
+  Axyz[2] = (float) az;
+
+  //apply offsets and scale factors from Magneto
+  /*
+  for (int i = 0; i < 3; i++) Axyz[i] = (Axyz[i] - A_cal[i]) * A_cal[i + 3]; ///PISTIN ITSE LAINUKSIIN!!!!!!!
+
+  Gxyz[0] = ((float) gx - G_off[0]) * gscale; //250 LSB(d/s) default to radians/s
+  Gxyz[1] = ((float) gy - G_off[1]) * gscale;
+  Gxyz[2] = ((float) gz - G_off[2]) * gscale;
+ */
+  now = micros();
+  deltat = (now - last) * 1.0e-6; //seconds since last update
+  last = now;
+
+  Mahony_update(Axyz[0], Axyz[1], Axyz[2], Gxyz[0], Gxyz[1], Gxyz[2], deltat);
+
+  roll  = atan2((q[0] * q[1] + q[2] * q[3]), 0.5 - (q[1] * q[1] + q[2] * q[2]));
+  pitch = asin(2.0 * (q[0] * q[2] - q[1] * q[3]));
+  //conventional yaw increases clockwise from North. Not that the MPU-6050 knows where North is.
+  yaw   = -atan2((q[1] * q[2] + q[0] * q[3]), 0.5 - (q[2] * q[2] + q[3] * q[3]));
+  // to degrees
+  yaw   *= 180.0 / PI;
+  if (yaw < 0) yaw += 360.0; //compass circle
+  pitch *= 180.0 / PI;
+  roll *= 180.0 / PI;
+
+  now_ms = millis(); //time to print?
+  if (now_ms - last_ms >= print_ms) {
+    last_ms = now_ms;
+    // print angles for serial plotter...
+    //  Serial.print("ypr ");
+ /*   Serial.print(yaw, 0);
+    Serial.print(", ");
+    Serial.print(pitch, 0);
+    Serial.print(", ");
+    Serial.println(roll, 0); */
+    Serial.print(q[0], 2);
+    Serial.print(", ");
+    Serial.print(q[1], 2);
+    Serial.print(", ");
+    Serial.print(q[2], 2);
+    Serial.print(", ");
+    Serial.print(q[3], 2);
+    Serial.print("\n ");
+  }
+
+  DataPackage package;
+  package.x = q[0];
+  package.y = q[1];
+  package.z = q[2];
+  package.w = q[3];
+  package.buttons;
+
+  uint8_t* ch;
+  DataPackage* packagePTR = (DataPackage*)malloc(sizeof(DataPackage));
+  packagePTR = &package;
+
+  ch = (uint8_t*)packagePTR;
+
+  pCharacteristic->setValue(ch,sizeof(DataPackage));
+  pCharacteristic->notify();
+  
+  delay(100);
+}
+
+void Mahony_update(float ax, float ay, float az, float gx, float gy, float gz, float deltat) {
+  float recipNorm;
+  float vx, vy, vz;
+  float ex, ey, ez;  //error terms
+  float qa, qb, qc;
+  static float ix = 0.0, iy = 0.0, iz = 0.0;  //integral feedback terms
+  float tmp;
+
+  // Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+  tmp = ax * ax + ay * ay + az * az;
+  if (tmp > 0.0)
+  {
+
+    // Normalise accelerometer (assumed to measure the direction of gravity in body frame)
+    recipNorm = 1.0 / sqrt(tmp);
+    ax *= recipNorm;
+    ay *= recipNorm;
+    az *= recipNorm;
+
+    // Estimated direction of gravity in the body frame (factor of two divided out)
+    vx = q[1] * q[3] - q[0] * q[2];
+    vy = q[0] * q[1] + q[2] * q[3];
+    vz = q[0] * q[0] - 0.5f + q[3] * q[3];
+
+    // Error is cross product between estimated and measured direction of gravity in body frame
+    // (half the actual magnitude)
+    ex = (ay * vz - az * vy);
+    ey = (az * vx - ax * vz);
+    ez = (ax * vy - ay * vx);
+
+    // Compute and apply to gyro term the integral feedback, if enabled
+    if (Ki > 0.0f) {
+      ix += Ki * ex * deltat;  // integral error scaled by Ki
+      iy += Ki * ey * deltat;
+      iz += Ki * ez * deltat;
+      gx += ix;  // apply integral feedback
+      gy += iy;
+      gz += iz;
+    }
+
+    // Apply proportional feedback to gyro term
+    gx += Kp * ex;
+    gy += Kp * ey;
+    gz += Kp * ez;
+  }
+
+  // Integrate rate of change of quaternion, q cross gyro term
+  deltat = 0.5 * deltat;
+  gx *= deltat;   // pre-multiply common factors
+  gy *= deltat;
+  gz *= deltat;
+  qa = q[0];
+  qb = q[1];
+  qc = q[2];
+  q[0] += (-qb * gx - qc * gy - q[3] * gz);
+  q[1] += (qa * gx + qc * gz - q[3] * gy);
+  q[2] += (qa * gy - qb * gz + q[3] * gx);
+  q[3] += (qa * gz + qb * gy - qc * gx);
+
+  // renormalise quaternion
+  recipNorm = 1.0 / sqrt(q[0] * q[0] + q[1] * q[1] + q[2] * q[2] + q[3] * q[3]);
+  q[0] = q[0] * recipNorm;
+  q[1] = q[1] * recipNorm;
+  q[2] = q[2] * recipNorm;
+  q[3] = q[3] * recipNorm;
+}
